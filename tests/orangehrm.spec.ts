a0f436e0ae9b2dc7859d91c0ce1441284a54668f
ravi9766607566 <ravisharma183@live.com>
2026-09-10 23:29:29 +0530

    Login

---
+++ tests/orangehrm.spec.ts
@@ -0,0 +1,7 @@
+import {test} from '@playwright/test'
+test("TC001 -Login Test", async ({page}) => {
+    await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+    await page.getByPlaceholder('Username').fill('Admin');
+    await page.getByPlaceholder('Password').fill('admin123');
+    await page.getByRole('button', { name: 'Login' }).click();
+})
---
+++ tests/saucedemo.spec.ts
@@ -0,0 +1,10 @@
+import{test} from '@playwright/test'
+test("Login test", async ({page}) => {
+
+   await page.goto("https://www.saucedemo.com/");
+   await page.getByPlaceholder('Username').fill('standard_user');
+   await page.getByPlaceholder('Password').fill('secret_sauce');
+   await page.getByRole('button').click();
+    
+
+})
